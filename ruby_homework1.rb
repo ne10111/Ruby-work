2719737c56960f31399664fdd8fe8a9ac3f9e0d8
neo maredi <neomaredi@gmail.com>
2015-06-22 18:36:33 -0400

    my ruby homework

---
+++ ruby_homework1.rb
@@ -0,0 +1,38 @@
+def only_in_america(str)
+	str + "only in america"
+
+end
+puts only_in_america("badfood")
+
+test_array =[100,10-1000]
+def find_max_in_array(some_array)
+	some_array.max 
+end
+
+find_max_in_array(test_array)
+
+def combines_arrays_in_hash(a1,a2)
+	new_hash = {}
+
+	for i in 0..at.length
+		my_key =al[i]
+		new_hash[key] = a2[i]
+	end
+end
+
+def number_one_to_hundred
+	for i in 1..100
+		myfloat = i.to_f
+		if (myfloat % 3==0 ) && (myfloat % 5==0 )
+			puts "#{myfloat} FizzBuzz"
+		elsif myfloat % 3 ==0
+		puts " #{myfloat} Fizz"
+	    elsif  myfloat % 5==0
+	    puts "#{myfloat} Buzz"
+		
+	    end
+    end
+
+end
+puts number_one_to_hundred
+ 
